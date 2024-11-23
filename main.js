b9023d0248dc2072fc54c33f1ba48a262617ef36
Saurabh <ashgamming806@gmail.com>
2024-11-23 12:47:48 +0530

    update

--- main.js
+++ main.js
@@ -1,122 +1,132 @@
-//Steps
-//create variables
-//generate random word function
-//add word to dom function create and call
-//create event listerner
-//create updateScoreFunction
-//create start Count down variables
-//create update time function
-//create game over function
-//difficulty working start
+//1
+const balance = document.getElementById(
+  "balance"
+);
+const money_plus = document.getElementById(
+  "money-plus"
+);
+const money_minus = document.getElementById(
+  "money-minus"
+);
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 },
+// ];
 
-const word=document.getElementById('word');
-const text = document.getElementById('text');
-const scoreEl = document.getElementById('score');
-const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('setting-btn');
-const settings = document.getElementById('settings');
-const settingForm = document.getElementById('settings-form');
-const difficultySelect = document.getElementById('difficulty');
-//List of Words
-const words = ['sigh','tense','airplane','HeyYou','whatsUp','welcome','brother','shape','grandLeaks','masterHouse','LifeRules','GoNext','Brother'];
+// let transactions = dummyTransactions;
 
+//last 
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 
-//1 after added variables
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
-//Initialize word
-let randomWord;
-
-//Initialize score,time
-let score =0;
-let time= 10;
-
-//all Difficulty
-//set difficulty
-let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty'): 'medium';
-
-//set difficulty select value
-difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty'): 'medium';
-
-//Focus on Input
-text.focus();
-
-//start Counting down
-const timeInterval = setInterval(updateTime ,1000)
-
-//generate random word
-function getRandomWord(){
-    return words[Math.floor(Math.random() * words.length)];
-}
-
-//add word to DOM
-function addWordToDOM(){
-    randomWord = getRandomWord();
-    word.innerHTML = randomWord;
-}
-
-addWordToDOM();
-
-//after calling in Event Listerner
-function updateScore(){
-    score++;
-    scoreEl.innerHTML = score;
-
-    if(time === 0){
-        clearInterval(timeInterval);
-        gameOver();
+//5
+//Add Transaction
+function addTransaction(e){
+  e.preventDefault();
+  if(text.value.trim() === '' || amount.value.trim() === ''){
+    alert('please add text and amount')
+  }else{
+    const transaction = {
+      id:generateID(),
+      text:text.value,
+      amount:+amount.value
     }
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value='';
+    amount.value='';
+  }
 }
 
-//game over function
-function gameOver(){
-    endgameEl.innerHTML = `
-    <h1>Time Ran Out</h1>
-    <p>Your final Score is ${score}</p>
-    <button onclick="location.reload()">Reload</button>
+
+//5.5
+//Generate Random ID
+function generateID(){
+  return Math.floor(Math.random()*1000000000);
+}
+
+//2
+
+//Add Trasactions to DOM list
+function addTransactionDOM(transaction) {
+  //GET sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+
+  //Add Class Based on Value
+  item.classList.add(
+    transaction.amount < 0 ? "minus" : "plus"
+  );
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
-    endgameEl.style.display = 'flex';
+  list.appendChild(item);
 }
 
-//update time
-function updateTime(){
-    time--;
-    timeEl.innerHTML = time + 's';
-    if(time === 0){
-        clearInterval(timeInterval);
-        //
-        gameOver();
-    }
+//4
+
+//Update the balance income and expence
+function updateValues() {
+  const amounts = transactions.map(
+    (transaction) => transaction.amount
+  );
+  const total = amounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense =
+    (amounts
+      .filter((item) => item < 0)
+      .reduce((acc, item) => (acc += item), 0) *
+    -1).toFixed(2);
+
+    balance.innerText=`$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
 }
 
-//Event Listerner
-text.addEventListener('input',e => {
-    const insertedText = e.target.value;
-    if(insertedText === randomWord){
-        addWordToDOM();
-        updateScore();
-        //clear 
-        e.target.value ='';
-        // difficulty
-        if(difficulty === 'hard'){
-            time+=2
-        }else if(difficulty === 'medium'){
-            time +=3;
-        }else{
-            time += 4;
-        }
-        updateTime();
-    }
-})
 
-//Difficulty event listerners
+//6 
 
-//settings btn 
-settingsBtn.addEventListener('click',() => settings.classList.toggle('hide'));
+//Remove Transaction by ID
+function removeTransaction(id){
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  Init();
+}
+//last
+//update Local Storage Transaction
+function updateLocalStorage(){
+  localStorage.setItem('transactions',JSON.stringify(transactions));
+}
 
-//settings select
-settingForm.addEventListener('change',e => {
-    difficulty = e.target.value;
-    localStorage.setItem('difficulty',difficulty);
-})
+//3
 
+//Init App
+function Init() {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+Init();
+
+form.addEventListener('submit',addTransaction);
